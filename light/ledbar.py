2a44b8a4b36d4e0e517b6c23887049c5cd3a215a
Ricky Moorhouse <ricky@samespirit.net>
2025-04-18 14:48:10 +0100

    enhance API

--- light/ledbar.py
+++ light/ledbar.py
@@ -3,12 +3,18 @@ import cherrypy
 try:
     import ledshim
     ledshim.set_clear_on_exit()
-except: 
+except ImportError: 
     print("ledshim not found - local dev")
+
+# Global variable to store the current color
+current_color = {"r": 0, "g": 0, "b": 0}
 
 
 def set_light(r, g, b):
-    print('Setting Blinkt to {r},{g},{b}'.format(r=r, g=g, b=b))
+    """Set the LED color."""
+    global current_color
+    current_color = {"r": r, "g": g, "b": b}
+    print(f'Setting LEDs to {r},{g},{b}')
     try:
         ledshim.set_clear_on_exit(False)
         ledshim.set_all(r, g, b)
@@ -16,16 +22,63 @@ def set_light(r, g, b):
     except:
         print("ledshim not found - local dev")
 
+def clear_light():
+    """Clear the LEDs."""
+    global current_color
+    current_color = {"r": 0, "g": 0, "b": 0}
+    print("Clearing LEDs")
+    try:
+        ledshim.clear()
+        ledshim.show()
+    except:
+        print("ledshim not found - local dev")
 
 
-set_light(255,255,255)
 
-class HelloWorld(object):
+class LEDController(object):
     @cherrypy.expose
-    def index(self, r=0, g=0, b=0):
-        set_light(int(r), int(g), int(b))
-        return "Hello World! " + str(r) + ","+str(g)+","+str(b)+""
+    @cherrypy.tools.json_out()
+    def index(self):
+        """Default endpoint."""
+        return {"message": "Welcome to the LED Controller API"}
 
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def set_rgb(self, r, g, b):
+        """Set the LED color using RGB values."""
+        set_light(int(r), int(g), int(b))
+        return {"status": "success", "color": current_color}
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def set_hex(self, hex_color):
+        """Set the LED color using a HEX value."""
+        if len(hex_color) != 6:
+            return {"status": "error", "message": "Invalid HEX color"}
+        try:
+            r = int(hex_color[0:2], 16)
+            g = int(hex_color[2:4], 16)
+            b = int(hex_color[4:6], 16)
+            set_light(r, g, b)
+            return {"status": "success", "color": current_color}
+        except ValueError:
+            return {"status": "error", "message": "Invalid HEX color"}
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def clear(self):
+        """Clear the LEDs."""
+        clear_light()
+        return {"status": "success", "message": "LEDs cleared"}
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def get_color(self):
+        """Get the current LED color."""
+        return {"status": "success", "color": current_color}
+
+
+# Configure and start the CherryPy server
 cherrypy.server.socket_host = '0.0.0.0'
 cherrypy.server.socket_port = 8080
-cherrypy.quickstart(HelloWorld())
+cherrypy.quickstart(LEDController())
